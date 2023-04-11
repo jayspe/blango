9098a48a1f04a924be00d12f7eb4f4cb6b70900e
Jansenio A <jansenio.amado@gmail.com>
2023-04-11 02:50:19 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+
 from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render
@@ -7,3 +9,7 @@ def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts})
     
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
