39209435d091bfe4f6a53f4722cdf4379782a2fd
Jansenio A <jansenio.amado@gmail.com>
2023-04-13 19:40:23 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -40,6 +40,12 @@ class Dev(Configuration):
   CSRF_COOKIE_SAMESITE = 'None'
   SESSION_COOKIE_SAMESITE = 'None'
 
+#   PASSWORD_HASHERS = [
+#       'django.contrib.auth.hashers.Argon2PasswordHasher',
+#       'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+#       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+#       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+#   ]
 
   # Application definition
 
@@ -127,6 +133,12 @@ class Dev(Configuration):
       },
   ]
 
+  PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
 
   # Internationalization
   # https://docs.djangoproject.com/en/3.2/topics/i18n/
