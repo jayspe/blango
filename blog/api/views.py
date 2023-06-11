61b81f9eda7933ad9e83ff5161e61308258eae45
Jansenio A <jansenio.amado@gmail.com>
2023-06-11 21:04:45 +0000

    Finish pagination and django filter

--- blog/api/views.py
+++ blog/api/views.py
@@ -28,6 +28,8 @@ from django.utils import timezone
 from datetime import timedelta
 from django.http import Http404
 
+from blog.api.filters import PostFilterSet
+
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
@@ -46,6 +48,12 @@ class TagViewSet(viewsets.ModelViewSet):
     @action(methods=["get"], detail=True, name="Posts with the Tag")
     def posts(self, request, pk=None):
         tag = self.get_object()
+        page = self.paginate_queryset(tag.posts)
+        if page is not None:
+            post_serializer = PostSerializer(
+                page, many=True, context={"request": request}
+            )
+            return self.get_paginated_response(post_serializer.data)
         post_serializer = PostSerializer(
             tag.posts, many=True, context={"request": request}
         )
@@ -61,6 +69,10 @@ class TagViewSet(viewsets.ModelViewSet):
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    ordering_fields = ["published_at", "author", "title", "slug"]
+    class PostViewSet(viewsets.ModelViewSet):
+        filterset_class = PostFilterSet
+
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
 
@@ -77,9 +89,16 @@ class PostViewSet(viewsets.ModelViewSet):
         if request.user.is_anonymous:
             raise PermissionDenied("You must be logged in to see which Posts are yours")
         posts = self.get_queryset().filter(author=request.user)
+
+        page = self.paginate_queryset(posts)
+
+        if page is not None:
+            serializer = PostSerializer(page, many=True, context={"request": request})
+            return self.get_paginated_response(serializer.data)
+
         serializer = PostSerializer(posts, many=True, context={"request": request})
         return Response(serializer.data)
-
+        
     @method_decorator(cache_page(120))
     def list(self, *args, **kwargs):
         return super(PostViewSet, self).list(*args, **kwargs)
